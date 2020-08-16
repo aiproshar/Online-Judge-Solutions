f3d65e11fb428891e0b40d9b09a70bd65ac1ea9d
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-08-17 02:36:53 +0600

    Much cleaner code, removed unnecessary variables

--- Leetcode/maximum-subarray.cpp
+++ Leetcode/maximum-subarray.cpp
@@ -6,18 +6,16 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int last_maximum = 0;
+        int current_end_maximum = 0;
         int global_maximum = nums[0];
         for(int i = 0 ; i < nums.size(); i++)
         {
-            int current_end_maximum = max(nums[i], (nums[i] + last_maximum));
+            int current_end_maximum = max(nums[i], (nums[i] + current_end_maximum));
             if(current_end_maximum > global_maximum)
             {
                 global_maximum = current_end_maximum;
             }
-            last_maximum = current_end_maximum;
         }
-
         return global_maximum;
     }
 };
