45d4c5177e05a378ad14125c840fbc0a760beec5
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-09-25 21:59:57 +0600

    Update 145. Binary Tree Postorder Traversal.cpp

--- LeetCode/145. Binary Tree Postorder Traversal.cpp
+++ LeetCode/145. Binary Tree Postorder Traversal.cpp
@@ -12,6 +12,7 @@
 
 //Trivial Recursive
 //Reference and Explanation : 94 and 144
+
 class Solution {
 public:
     vector<int> ans;
@@ -35,4 +36,40 @@ public:
     }
 };
 //Iterative Solution
-//Reference and Explanation : 94 and 144
+//2 stack method, more complex than pre/in
+//Intermediate stack and Ans stack
+//Explanation : https://www.youtube.com/watch?v=qT65HltK2uE 
+//There is some one stack bullshit hard implementation, not interested :P 
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s1;
+        stack<TreeNode*> s2;
+        if(root == nullptr) return ans;
+        s1.push(root);
+        while(!s1.empty())
+        {
+            root = s1.top();
+            s1.pop();
+            s2.push(root);
+            if(root->left != nullptr)
+            {
+                s1.push(root->left);
+            }
+            if(root->right != nullptr)
+            {
+                s1.push(root->right);
+            }
+        }
+        while(!s2.empty())
+        {
+            auto temp = s2.top();
+            s2.pop();
+            ans.push_back(temp->val);
+        }
+        return ans;
+    }
+};
+
