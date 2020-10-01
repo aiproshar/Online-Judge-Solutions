417cd668b7953da2eb5077205343031e91a9b47a
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-10-02 04:17:39 +0600

    Update 106. Construct Binary Tree from Inorder and Postorder Traversal.cpp

--- LeetCode/106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ LeetCode/106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -22,6 +22,7 @@
 //5. Recurse on LST and RST
 //Check if list is empty(no element on that subtree), return nullptr
 //Solution is not optimal, because vector copy overhead. (bad runtime + excessive memory usage)
+//Jenny mams explanation: https://www.youtube.com/watch?v=s5XRtcud35E
 //More optimal Solution: Better if we define our own function and just pass the start, end location of vectors in call stack(slicing behaviour like NumPy)
 class Solution {
 public:
@@ -47,5 +48,47 @@ public:
         root->left = buildTree(lst_inorder, lst_postorder);
         root->right = buildTree(rst_inorder, rst_postorder);
         return root;
+    }
+};
+
+//Optimized Solution
+//No call stack vector copy overhead
+//Just keep track of index
+//Class Space copy inorder and postorder list, so accesable from any call stack
+//We need size information for end offset (start + sz - 1)
+//First solution : 240ms/162 MB, optimized solution: 28 ms, 27 MB (10x improvement)
+class Solution {
+    vector<int> inorder;
+    vector<int> postorder;
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        Solution::inorder = inorder;
+        Solution::postorder = postorder;
+        return optimized_buildTree(0, inorder.size() - 1, 0, postorder.size() - 1);
+    }
+    TreeNode* optimized_buildTree(int inorder_start, int inorder_end, int post_start, int post_end)
+    {
+        if(inorder_end < inorder_start || post_end < post_start)
+            return nullptr;
+        TreeNode* root = new TreeNode(postorder[post_end]);
+        int lst_inorder_start , lst_inorder_end, rst_inorder_start, rst_inorder_end, lst_post_start, lst_post_end, rst_post_start, rst_post_end;
+        
+        int root_idx;
+        int sz = 0;
+        for(root_idx = inorder_start; inorder[root_idx] != root->val; root_idx++)
+            sz++;
+        lst_inorder_start = inorder_start;
+        lst_inorder_end = root_idx - 1;
+        lst_post_start = post_start;
+        lst_post_end = post_start + sz - 1;
+        
+        rst_inorder_start = root_idx + 1;
+        rst_inorder_end = inorder_end;
+        rst_post_start = lst_post_end + 1;
+        rst_post_end = post_end - 1;
+        root->left = optimized_buildTree(lst_inorder_start, lst_inorder_end, lst_post_start, lst_post_end);
+        root->right = optimized_buildTree(rst_inorder_start, rst_inorder_end, rst_post_start, rst_post_end);
+        return root;
+
     }
 };
