f45d9982ffcea7c61bcd6399fd686396355d0a3f
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-10-04 09:03:58 +0600

    Create 236. Lowest Common Ancestor of a Binary Tree.cpp

---
+++ LeetCode/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+//recursive approach
+//2 case,1. our current node has one value, and another in in either LST or RST
+//2. We have one in LST and another in RST
+//termination flag for runtime optimization (if we already have ans, why recurse any more!)
+//If both values are in either LST or RST, its not an ancestor
+class Solution {
+public:
+    TreeNode* ans;
+    bool terminate = false;
+    bool found(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if(!root || terminate)
+            return 0;
+        bool lst_found = found(root->left, p, q);
+        bool rst_found = found(root->right,p, q);
+        if(root->val == p->val || root->val == q->val)
+        {
+            if(lst_found || rst_found)
+            {
+                ans = root;
+                terminate = true;
+                return false;
+            }
+            else
+                return true;
+        }
+        else if(lst_found && rst_found)
+        {
+            ans = root;
+            terminate = true;
+            return false;
+        }
+        return lst_found || rst_found;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        found(root, p,q);
+        return ans;
+    }
+};
