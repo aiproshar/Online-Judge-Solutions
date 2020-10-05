86ae3a8a7bffc498b70c7392141115aed23a81f8
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-10-05 06:07:28 +0600

    Create 297. Serialize and Deserialize Binary Tree.cpp

---
+++ LeetCode/297. Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+//Hard Leetcod Problem
+//Recursive preorder traversal list (string) buildup
+//'#' as indication of nullpointers for childrens of leaf nodes and parent with one child
+//',' to separate out values
+//Deserializing : Same recursive approach (bottom up)
+//Pass index reference to indicate start location
+//String reference for less call stack overhead
+//Beats 94%
+//Iterative version might be asked as follow up
+//Will try to do it later :)
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string datastream;
+        if(!root)
+            return datastream;
+        preorder_stream(root, datastream);
+        return datastream;
+    }
+    void preorder_stream(TreeNode* root, string& datastream)
+    {
+        if(root == nullptr)
+        {
+            datastream.push_back('x');
+            datastream.push_back(',');
+            return;
+        }
+        datastream += to_string(root->val);
+        datastream.push_back(',');
+        preorder_stream(root->left, datastream);
+        preorder_stream(root->right, datastream);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(!data.size())
+            return nullptr;
+        int start = 0;
+        return build_tree(data, start);
+    }
+    TreeNode* build_tree(string& data, int& start_pos)
+    {
+        if(data[start_pos] == 'x')
+            return nullptr;
+        else
+        {
+            string temp;
+            while(data[start_pos] != ',')
+            {
+                temp.push_back(data[start_pos]);
+                start_pos++;
+            }
+            TreeNode* root = new TreeNode(stoi(temp));
+            start_pos++;
+            root->left = build_tree(data, start_pos);
+            start_pos++;
+            while(data[start_pos] != ',')
+                start_pos++;
+            start_pos++;
+            root->right = build_tree(data, start_pos);
+            return root;
+        }        
+    }   
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
