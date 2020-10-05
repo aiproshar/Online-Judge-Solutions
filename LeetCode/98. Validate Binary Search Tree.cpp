4963b040b46fa2f43b66da27ee52802772781f05
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-10-06 04:57:04 +0600

    Create 98. Validate Binary Search Tree.cpp

---
+++ LeetCode/98. Validate Binary Search Tree.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//Recursive Solution
+//In order traversal
+//More optimization can be made by setting Flags (we got answer that its not BST, abort further recursion)
+class Solution {
+    vector<int> list;
+public:
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+        for(int i = 1; i < list.size(); i++)
+        {
+            if(list[i-1] < list[i])
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+    void inorder(TreeNode* root)
+    {
+        if(!root)
+            return;
+        inorder(root->left);
+        list.push_back(root->val);
+        inorder(root->right);
+    }
+};
+//Iterative solution
+//Iterative inorder traversal
+//No call stack overhead
+//Ref: {94. Binary Tree Inorder Traversal}
+class Solution {
+    vector<int> list;
+public:
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+        for(int i = 1; i < list.size(); i++)
+        {
+            if(list[i-1] < list[i])
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+    void inorder(TreeNode* root)
+    {
+        stack<TreeNode*> S;
+        while(1)
+        {
+            if(root != nullptr)
+            {
+                S.push(root);
+                root = root->left;
+            }
+            else
+            {
+                if(S.empty())
+                    break;
+                root = S.top();
+                S.pop();
+                list.push_back(root->val);
+                root = root->right;
+            }
+        }
+    }
+};
